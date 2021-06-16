04b78efe0cf0e845d6359fa5cda8695d36187427
Tran Duc Y <ducyk41cntt@gmail.com>
2021-06-16 15:37:19 +0700

    fix feedback

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 
   wrapperMenu.addEventListener('click', function(){
     $('.menu-responsive').toggleClass('menu-open');
+    $('.wrapper-menu').toggleClass('menu-active');
   })
 
 })
